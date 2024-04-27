9b52629ba77546e1159752760f43ead1d8f67aba
springhana <xksxksanfro7@naver.com>
2024-04-28 08:50:55 +0900

    ✨feat:  로그아웃 추가

--- src/screens/map/MapHomeScreen.tsx
+++ src/screens/map/MapHomeScreen.tsx
@@ -1,9 +1,13 @@
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
+import useAuth from '@/hooks/queries/useAuth';
 
 function MapHomeScreen() {
+  const {logoutMutation} = useAuth();
+
   return (
     <View>
       <Text>맵 스크린</Text>
+      <Button title="로그아웃" onPress={() => logoutMutation.mutate(null)} />
     </View>
   );
 }
