22d42173f6428965cc9c320bd61e1692d1d05596
springhana <xksxksanfro7@naver.com>
2024-04-28 08:50:02 +0900

    ✨feat:  인증 데이터 타입 정의

---
+++ src/types/common.ts
@@ -0,0 +1,31 @@
+import {
+  UseMutationOptions,
+  UseQueryOptions,
+  QueryKey,
+} from '@tanstack/react-query';
+import {AxiosError} from 'axios';
+
+type ResponseError = AxiosError<{
+  statusCode: string;
+  message: string;
+  error: string;
+}>;
+
+type UseMutationOptionsCustomOptions<
+  TData = unknown,
+  TVariables = unknown,
+> = Omit<
+  UseMutationOptions<TData, ResponseError, TVariables, unknown>,
+  'mutationFn'
+>;
+
+type UseQueryCustomOptions<TQueryFnData = unknown, TData = TQueryFnData> = Omit<
+  UseQueryOptions<TQueryFnData, ResponseError, TData, QueryKey>,
+  'queryKey'
+>;
+
+export type {
+  ResponseError,
+  UseMutationOptionsCustomOptions,
+  UseQueryCustomOptions,
+};
