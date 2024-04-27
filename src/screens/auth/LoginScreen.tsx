850bfd0169bfbdcce08f75a697660b289c55f13b
springhana <xksxksanfro7@naver.com>
2024-04-28 08:49:04 +0900

    🔨modify:  로그인 추가

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,41 +1,16 @@
+import {useRef} from 'react';
 import {SafeAreaView, StyleSheet, View} from 'react-native';
-import InputField from '../../components/InputField';
-import CustomButton from '../../components/CustomButton';
-import useForm from '../../hooks/useForm';
-import {validateLogin} from '../../utils';
+
+import InputField from '@/components/InputField';
+import CustomButton from '@/components/CustomButton';
+import useForm from '@/hooks/useForm';
+import {validateLogin} from '@/utils';
+import {TextInput} from 'react-native-gesture-handler';
+import useAuth from '@/hooks/queries/useAuth';
 
 function LoginScreen() {
-  // const [email, setEmail] = useState('');
-  // const [password, setPassWord] = useState('');
-
-  // const handleChangeEmail = (text: string) => {
-  //   setEmail(text);
-  // };
-
-  // const handleChangePassWord = (text: string) => {
-  //   setPassWord(text);
-  // };
-
-  // const [values, setValues] = useState({
-  //   email: '',
-  //   password: '',
-  // });
-  // const [touched, setTouched] = useState({
-  //   email: false,
-  //   password: false,
-  // });
-  // const handleChangeText = (name: string, text: string) => {
-  //   setValues({
-  //     ...values,
-  //     [name]: text,
-  //   });
-  // };
-  // const handleBlur = (name: string) => {
-  //   setTouched({
-  //     ...touched,
-  //     [name]: true,
-  //   });
-  // };
+  const passwordRef = useRef<TextInput | null>(null);
+  const {loginMutation} = useAuth();
 
   const login = useForm({
     initialValue: {email: '', password: ''},
@@ -43,30 +18,32 @@ function LoginScreen() {
   });
 
   const handleSubmit = () => {
-    console.log('values', login.values);
+    loginMutation.mutate(login.values);
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
         <InputField
+          autoFocus
           placeholder="이메일"
           error={login.errors.email}
           touched={login.touched.email}
           inputMode="email"
-          // value={values.email}
-          // onChangeText={text => handleChangeText('email', text)}
-          // onBlur={() => handleBlur('email')}
+          blurOnSubmit={false}
+          returnKeyType="next"
+          onSubmitEditing={() => passwordRef.current?.focus()}
           {...login.getTextInputProps('email')}
         />
         <InputField
+          ref={passwordRef}
           placeholder="비밀번호"
           error={login.errors.password}
           touched={login.touched.password}
           secureTextEntry
-          // value={values.password}
-          // onChangeText={text => handleChangeText('password', text)}
-          // onBlur={() => handleBlur('password')}
+          blurOnSubmit={false}
+          returnKeyType="join"
+          onSubmitEditing={handleSubmit}
           {...login.getTextInputProps('password')}
         />
       </View>
