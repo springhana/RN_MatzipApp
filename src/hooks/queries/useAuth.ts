6f4c38be9e917c71a42c00ec797a063496c0821b
springhana <xksxksanfro7@naver.com>
2024-04-28 08:46:42 +0900

    ✨feat:  인증 요청 구현

---
+++ src/hooks/queries/useAuth.ts
@@ -0,0 +1,113 @@
+import {useMutation, useQuery} from '@tanstack/react-query';
+import {
+  getAccessToken,
+  getProfile,
+  logout,
+  postLogin,
+  postSignup,
+} from '../../api/auth';
+import {
+  UseMutationOptionsCustomOptions,
+  UseQueryCustomOptions,
+} from '../../types/common';
+import {removeEncryptStorage, setEncryptStorage} from '../../utils';
+import {removeHeader, setHeadere} from '../../utils/header';
+import {useEffect} from 'react';
+import queryClient from '../../api/queryClient';
+import {numbers, queryKeys, storageKeys} from '../../constants';
+
+function useSignup(mutaionOptions?: UseMutationOptionsCustomOptions) {
+  return useMutation({
+    mutationFn: postSignup,
+    ...mutaionOptions,
+  });
+}
+
+function useLogin(mutaionOptions?: UseMutationOptionsCustomOptions) {
+  return useMutation({
+    mutationFn: postLogin,
+    onSuccess: ({accessToken, refreshToken}) => {
+      setEncryptStorage(storageKeys.REFRESH_TOKEN, refreshToken);
+      setHeadere('Authorization', `Bearer ${accessToken}`);
+    },
+    onSettled: () => {
+      queryClient.refetchQueries({
+        queryKey: [queryKeys.AUTH, queryKeys.GET_ACCESS_TOKEN],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [queryKeys.AUTH, queryKeys.GET_PROFILE],
+      });
+    },
+    ...mutaionOptions,
+  });
+}
+
+function useGetRefreshToken() {
+  const {isSuccess, data, isError} = useQuery({
+    queryKey: [queryKeys.AUTH, queryKeys.GET_ACCESS_TOKEN],
+    queryFn: getAccessToken,
+    staleTime: numbers.ACCESS_TOKEN_REFRESH_TIME,
+    refetchInterval: numbers.ACCESS_TOKEN_REFRESH_TIME, // 시간 주기에 따라 refetch
+    refetchOnReconnect: true, // 자동 갱신 (자동 연결)
+    refetchIntervalInBackground: true, // 벡그라운드에서 refetch
+  });
+
+  useEffect(() => {
+    if (isSuccess) {
+      setHeadere('Authorization', `Bearer ${data.accessToken}`);
+      setEncryptStorage(storageKeys.REFRESH_TOKEN, data.refreshToken);
+    }
+  }, [isSuccess]);
+
+  useEffect(() => {
+    if (isError) {
+      removeHeader('Authorization');
+      removeEncryptStorage(storageKeys.REFRESH_TOKEN);
+    }
+  }, [isError]);
+
+  return {isSuccess, isError};
+}
+
+function useGetProfile(queryOptions?: UseQueryCustomOptions) {
+  return useQuery({
+    queryKey: [queryKeys.AUTH, queryKeys.GET_PROFILE],
+    queryFn: getProfile,
+    ...queryOptions,
+  });
+}
+
+function useLogout(mutationOptions?: UseMutationOptionsCustomOptions) {
+  return useMutation({
+    mutationFn: logout,
+    onSuccess: () => {
+      removeHeader('Authorization');
+      removeEncryptStorage(storageKeys.REFRESH_TOKEN);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({queryKey: [queryKeys.AUTH]});
+    },
+    ...mutationOptions,
+  });
+}
+
+function useAuth() {
+  const signupMutation = useSignup();
+  const refreshTokenQuery = useGetRefreshToken();
+  const getProfileQuery = useGetProfile({
+    enabled: refreshTokenQuery.isSuccess,
+  });
+  const isLogin = getProfileQuery.isSuccess;
+  const loginMutation = useLogin();
+  const logoutMutation = useLogout();
+
+  return {
+    signupMutation,
+    loginMutation,
+    isLogin,
+    getProfileQuery,
+    logoutMutation,
+  };
+}
+
+export default useAuth;
