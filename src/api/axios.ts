cda24aa6b9aef336b29b95c47369af141b9a4d43
springhana <xksxksanfro7@naver.com>
2024-04-28 08:47:05 +0900

    ✨feat:  통합 axios 구현

---
+++ src/api/axios.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+import {Platform} from 'react-native';
+
+const axiosInstance = axios.create({
+  baseURL:
+    Platform.OS === 'android'
+      ? 'http://10.0.2.2:3030' //http://192.168.68.52:3030
+      : 'http://localhost:3030',
+
+  withCredentials: true,
+});
+
+export default axiosInstance;
