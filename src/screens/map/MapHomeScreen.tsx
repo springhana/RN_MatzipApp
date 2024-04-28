4f26a6f0cedb445d406143810f9f7ab5a8f3369c
springhana <xksxksanfro7@naver.com>
2024-04-28 17:33:06 +0900

    ✨feat:  지도 맵 구현

--- src/screens/map/MapHomeScreen.tsx
+++ src/screens/map/MapHomeScreen.tsx
@@ -1,15 +1,104 @@
-import {View, Text, Button} from 'react-native';
-import useAuth from '@/hooks/queries/useAuth';
+import {StyleSheet, Pressable, View} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {colors} from '@/constants';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+import {CompositeNavigationProp, useNavigation} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {MapStackParamList} from '@/navigations/stack/MapStackNavigator';
+import {DrawerNavigationProp} from '@react-navigation/drawer';
+import {MainDrawerParamList} from '@/navigations/drawer/MainDrawerNavigator';
+import {useRef} from 'react';
+import useUserLocation from '@/hooks/useUserLocation';
+import usePermission from '@/hooks/usePermission';
+import mapStyle from '@/style/mapStyle';
+
+type Navigation = CompositeNavigationProp<
+  StackNavigationProp<MapStackParamList>,
+  DrawerNavigationProp<MainDrawerParamList>
+>;
 
 function MapHomeScreen() {
-  const {logoutMutation} = useAuth();
+  const inset = useSafeAreaInsets();
+  const navigation = useNavigation<Navigation>();
+  const mapRef = useRef<MapView | null>(null);
+  const {userLocation, isUserLocationError} = useUserLocation();
+  usePermission('LOCATION');
+
+  const handlePressUserLocation = () => {
+    if (isUserLocationError) {
+      return;
+    }
+    mapRef.current?.animateToRegion({
+      latitude: userLocation.latitude,
+      longitude: userLocation.longitude,
+      longitudeDelta: 0.0922,
+      latitudeDelta: 0.0421,
+    });
+  };
 
   return (
-    <View>
-      <Text>맵 스크린</Text>
-      <Button title="로그아웃" onPress={() => logoutMutation.mutate(null)} />
-    </View>
+    <>
+      <MapView
+        ref={mapRef}
+        style={styles.container}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        followsUserLocation
+        showsMyLocationButton={true}
+        customMapStyle={mapStyle}
+      />
+      <Pressable
+        style={[styles.drawerButton, {top: inset.top || 20}]}
+        onPress={() => navigation.openDrawer()}>
+        <Ionicons name="menu" color={colors.WHITE} size={25} />
+      </Pressable>
+      <View style={styles.buttonList}>
+        <Pressable style={styles.mapButton} onPress={handlePressUserLocation}>
+          <MaterialIcons name="my-location" color={colors.WHITE} size={25} />
+        </Pressable>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  drawerButton: {
+    position: 'absolute',
+    left: 0,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    backgroundColor: colors.PINK_700,
+    borderTopRightRadius: 50,
+    borderBottomRightRadius: 50,
+    shadowColor: colors.BKACK,
+    shadowOffset: {width: 1, height: 1},
+    shadowOpacity: 0.5, // 안드로이드에서 적용 안됨
+    elevation: 4,
+  },
+  buttonList: {
+    position: 'absolute',
+    bottom: 30,
+    right: 15,
+  },
+  mapButton: {
+    backgroundColor: colors.PINK_700,
+    marginVertical: 5,
+    height: 48,
+    width: 48,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    shadowColor: colors.BKACK,
+    shadowOffset: {width: 1, height: 2},
+    shadowOpacity: 0.5, // 안드로이드에서 적용 안됨
+    elevation: 2,
+  },
+});
 
 export default MapHomeScreen;
