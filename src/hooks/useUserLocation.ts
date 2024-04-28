03dc349b71b5baa6bae2033202fbc03ed79adfc0
springhana <xksxksanfro7@naver.com>
2024-04-28 17:29:55 +0900

    ✨feat:  현재 위치 기능 추가

---
+++ src/hooks/useUserLocation.ts
@@ -0,0 +1,34 @@
+import {useEffect, useState} from 'react';
+import {LatLng} from 'react-native-maps';
+
+import Geolocation from '@react-native-community/geolocation';
+import useAppState from './useAppState';
+
+function useUserLocation() {
+  const [userLocation, setUserLocation] = useState<LatLng>({
+    latitude: 37.5516032365118,
+    longitude: 126.98989626020192,
+  });
+  const [isUserLocationError, setIsUserLocationError] = useState(false);
+  const {isComback} = useAppState();
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      info => {
+        const {latitude, longitude} = info.coords;
+        setUserLocation({latitude, longitude});
+        setIsUserLocationError(false);
+      },
+      () => {
+        setIsUserLocationError(true);
+      },
+      {
+        enableHighAccuracy: true,
+      },
+    );
+  }, [isComback]);
+
+  return {userLocation, isUserLocationError};
+}
+
+export default useUserLocation;
