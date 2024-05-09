dc0bfa61f0f4217e913fec84a3ed940746ff094f
springhana <xksxksanfro7@naver.com>
2024-05-10 03:06:42 +0900

    🔨modify:  장소 추가 스크린 추가

--- src/navigations/stack/MapStackNavigator.tsx
+++ src/navigations/stack/MapStackNavigator.tsx
@@ -1,9 +1,12 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import {mapNavigations} from '@/constants';
 import MapHomeScreen from '@/screens/map/MapHomeScreen';
+import AddPostScreen from '@/screens/map/AddPostScreen';
+import {LatLng} from 'react-native-maps';
 
 export type MapStackParamList = {
   [mapNavigations.MAP_HOME]: undefined;
+  [mapNavigations.ADD_POST]: {location: LatLng};
 };
 
 const Stack = createStackNavigator<MapStackParamList>();
@@ -30,6 +33,13 @@ function MapStackNavigator() {
         options={{
           headerTitle: '',
           headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name={mapNavigations.ADD_POST}
+        component={AddPostScreen}
+        options={{
+          headerTitle: '장소 추가',
         }}
       />
     </Stack.Navigator>
