e25204dc88f9d8870d5a1b2b749f13ba56f9a6b7
springhana <xksxksanfro7@naver.com>
2024-05-10 03:00:28 +0900

    🔨modify:  쿼리 키 추가

--- src/constants/keys.ts
+++ src/constants/keys.ts
@@ -2,6 +2,10 @@ const queryKeys = {
   AUTH: 'auth',
   GET_ACCESS_TOKEN: 'getAccessToken',
   GET_PROFILE: 'getProfile',
+  MARKER: 'marker',
+  GET_MARKERS: 'getMarkers',
+  POST: 'Post',
+  GET_POST: 'getPost',
 } as const;
 
 const storageKeys = {
