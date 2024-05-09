7fd6789b8aab9c1f73c5e371088e9fc13c9e8db6
springhana <xksxksanfro7@naver.com>
2024-05-10 03:00:22 +0900

    🔨modify:  쿼리 키 추가

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,15 +1,22 @@
 const colors = {
   WHITE: '#FFF',
   PINK_200: '#FAE2E9',
+  PINK_400: '#EC87A5',
   PINK_500: '#BF5C79',
   PINK_700: '#C63B64',
   RED_300: '#FFB4B4',
   RED_500: '#FF5F5F',
+  BLUE_400: '#B4E0FF',
+  BLUE_500: '#0D8AFF',
+  GREEN_400: '#CCE6BA',
+  YELLOW_400: '#FFE594',
+  PURPLE_400: '#C4C4E7',
   GRAY_100: '#E8E8E8',
   GRAY_200: '#E7E7E7',
+  GRAY_300: '#D8D8D8',
   GRAY_500: '#8E8E8E',
   GRAY_700: '#575757',
-  BKACK: '#000',
+  BLACK: '#000',
 };
 
 export {colors};
