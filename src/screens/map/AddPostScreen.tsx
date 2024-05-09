39d97c7f1ab74cd88e7418d8f1d9d6f8be966935
springhana <xksxksanfro7@naver.com>
2024-05-10 02:58:10 +0900

    ✨feat:  AddPostScreen에 관련된 함수 추가

---
+++ src/screens/map/AddPostScreen.tsx
@@ -0,0 +1,176 @@
+import {useEffect} from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+} from 'react-native';
+import Octicons from 'react-native-vector-icons/Octicons';
+
+import {colors, mapNavigations} from '@/constants';
+import {MapStackParamList} from '@/navigations/stack/MapStackNavigator';
+import {StackScreenProps} from '@react-navigation/stack';
+import InputField from '@/components/InputField';
+import CustomButton from '@/components/CustomButton';
+import {useRef, useState} from 'react';
+import {TextInput} from 'react-native-gesture-handler';
+import useForm from '@/hooks/useForm';
+import {getDateWithSeparator, validateAddPost} from '@/utils';
+import AddPostHeaderRight from '@/components/AddPostHeaderRight';
+import useMutateCreatePost from '@/hooks/queries/useMutateCreatePost';
+import {MarkerColor} from '@/types/domain';
+import useGetAddress from '@/hooks/useGetAddress';
+import MarkerSelector from '@/components/MarkerSelector';
+import ScoreInput from '@/components/ScoreInput';
+import DataPickerOption from '@/components/DataPickerOption';
+import useModal from '@/hooks/useModal';
+import ImageInput from '@/components/ImageInput';
+import usePermission from '@/hooks/usePermission';
+import useImagePicker from '@/hooks/useImagePicker';
+import PreviewImageList from '@/components/PreviewImageList';
+
+type AddPostScreenProps = StackScreenProps<
+  MapStackParamList,
+  typeof mapNavigations.ADD_POST
+>;
+
+function AddPostScreen({route, navigation}: AddPostScreenProps) {
+  const {location} = route.params;
+
+  const descriptionRef = useRef<TextInput | null>(null);
+  const createPost = useMutateCreatePost();
+  const address = useGetAddress(location);
+  const addPost = useForm({
+    initialValue: {title: '', description: ''},
+    validate: validateAddPost,
+  });
+
+  const [markerColor, setMarkerColor] = useState<MarkerColor>('RED');
+  const [score, setScore] = useState(5);
+  const [date, setDate] = useState(new Date());
+  const [isPicked, setisPicked] = useState(false);
+  const datePickerModal = useModal();
+  const imagePicker = useImagePicker({initialImage: []});
+  usePermission('PHOTO');
+
+  const handleConfirmDate = () => {
+    setisPicked(true);
+    datePickerModal.hide();
+  };
+
+  const handleChangeDate = (pickedDate: Date) => {
+    setDate(pickedDate);
+  };
+
+  const handleSelectMarker = (name: MarkerColor) => {
+    setMarkerColor(name);
+  };
+
+  const handleChangeScore = (score: number) => {
+    setScore(score);
+  };
+
+  const handleSubmit = () => {
+    const body = {
+      date,
+      title: addPost.values.title,
+      description: addPost.values.description,
+      color: markerColor,
+      score,
+      imageUris: imagePicker.imageUris,
+    };
+    createPost.mutate(
+      {address, ...location, ...body},
+      {
+        onSuccess: () => navigation.goBack(),
+      },
+    );
+  };
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => AddPostHeaderRight(handleSubmit),
+    });
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.contentContainer}>
+        <View style={styles.inputContainer}>
+          <InputField
+            value={address}
+            disabled
+            icon={
+              <Octicons name="location" size={16} color={colors.GRAY_500} />
+            }
+          />
+          <CustomButton
+            variant="outlined"
+            size="large"
+            label={isPicked ? getDateWithSeparator(date, '. ') : '날짜 선택'}
+            onPress={datePickerModal.show}
+          />
+          <InputField
+            placeholder="제목을 입력하세요"
+            error={addPost.errors.title}
+            touched={addPost.touched.title}
+            blurOnSubmit={false}
+            returnKeyType="next"
+            onSubmitEditing={() => descriptionRef.current?.focus()}
+            {...addPost.getTextInputProps('title')}
+          />
+          <InputField
+            ref={descriptionRef}
+            placeholder="기록하고 싶은 내용을 입력하세요. (선택)"
+            error={addPost.errors.description}
+            touched={addPost.touched.description}
+            multiline
+            returnKeyType="next"
+            {...addPost.getTextInputProps('description')}
+          />
+          <MarkerSelector
+            score={score}
+            markerColor={markerColor}
+            onPressMarker={handleSelectMarker}
+          />
+          <ScoreInput score={score} onChangeScore={handleChangeScore} />
+          <View style={styles.imagesViewer}>
+            <ImageInput onChange={imagePicker.handleChange} />
+            <PreviewImageList
+              imageUris={imagePicker.imageUris}
+              onDelete={imagePicker.delete}
+              onChangeOrder={imagePicker.changeOrder}
+            />
+          </View>
+          <DataPickerOption
+            date={date}
+            isVisible={datePickerModal.isVisible}
+            onChangeDate={handleChangeDate}
+            onConfirmDate={handleConfirmDate}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 20,
+    marginBottom: 10,
+  },
+  inputContainer: {
+    gap: 20,
+    marginBottom: 20,
+  },
+  imagesViewer: {
+    flexDirection: 'row',
+  },
+});
+
+export default AddPostScreen;
