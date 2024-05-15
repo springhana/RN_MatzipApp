06385d1bb154914cd6d88617d02e9487c3cdb781
springhana <xksxksanfro7@naver.com>
2024-05-16 05:07:15 +0900

    ✨feat:  피드헤더 메뉴 버튼 구현

---
+++ src/components/FeedHomeHeaderLeft.tsx
@@ -0,0 +1,32 @@
+import {StyleSheet, Text, View} from 'react-native';
+import HeaderButton from './HeaderButton';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {colors} from '@/constants';
+import {CompositeNavigationProp} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {FeedStackParamList} from '@/navigations/stack/FeedStackNavigator';
+import {DrawerNavigationProp} from '@react-navigation/drawer';
+
+type FeedHomeHeaderLeftProps = CompositeNavigationProp<
+  StackNavigationProp<FeedStackParamList>,
+  DrawerNavigationProp<FeedStackParamList>
+>;
+
+function FeedHomeHeaderLeft(navigation: FeedHomeHeaderLeftProps) {
+  return (
+    <HeaderButton
+      icon={
+        <Ionicons
+          name="menu"
+          color={colors.BLACK}
+          size={25}
+          onPress={() => navigation.openDrawer()}
+        />
+      }
+    />
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default FeedHomeHeaderLeft;
