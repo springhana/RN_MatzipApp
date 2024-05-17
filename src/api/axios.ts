6d3794da6899dc1a101f50c4a7117cd92fa39bb5
springhana <xksxksanfro7@naver.com>
2024-05-18 04:12:07 +0900

    🔨modify:  주소 수정

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -5,7 +5,7 @@ import {Platform} from 'react-native';
 const axiosInstance = axios.create({
   baseURL:
     Platform.OS === 'android'
-      ? 'http://10.0.2.2:3030' //http://192.168.68.52:3030
+      ? 'http://192.168.68.52:3030' // http://192.168.68.52:3030, http://10.0.2.2:3030
       : 'http://localhost:3030',
 
   withCredentials: true,
