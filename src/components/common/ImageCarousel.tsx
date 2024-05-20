b387b8547bf391fa7c87eff4948cabc08a026cd3
springhana <xksxksanfro7@naver.com>
2024-05-21 02:38:48 +0900

    ✨feat:  이미지 캐러셀 추가

---
+++ src/components/common/ImageCarousel.tsx
@@ -0,0 +1,129 @@
+import {useState} from 'react';
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  Platform,
+  StyleSheet,
+  View,
+  Pressable,
+} from 'react-native';
+import Octicons from 'react-native-vector-icons/Octicons';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+
+import {colors} from '@/constants';
+import {ImageUri} from '@/types';
+import {useNavigation} from '@react-navigation/core';
+
+interface ImageCarouselProps {
+  images: ImageUri[];
+  pressedIndex?: number;
+}
+
+const deviceWidth = Dimensions.get('window').width;
+
+function ImageCarousel({images, pressedIndex = 0}: ImageCarouselProps) {
+  const inset = useSafeAreaInsets();
+  const navigation = useNavigation();
+  const [page, setPage] = useState(pressedIndex);
+  const [initialIndex, setInitialIndex] = useState(pressedIndex);
+
+  const handleScorll = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const newPage = Math.round(e.nativeEvent.contentOffset.x / deviceWidth);
+
+    setPage(newPage);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Pressable
+        style={[styles.backButton, {marginTop: inset.top + 10}]}
+        onPress={() => navigation.goBack()}>
+        <Octicons name="arrow-left" size={30} color={colors.WHITE} />
+      </Pressable>
+      <FlatList
+        data={images}
+        renderItem={({item}) => (
+          <View style={{width: deviceWidth}}>
+            <Image
+              style={styles.image}
+              source={{
+                uri: `${
+                  Platform.OS === 'ios'
+                    ? 'http://localhost:3030'
+                    : 'http://192.168.68.52:3030'
+                }/${item.uri}`,
+              }}
+              resizeMode="contain"
+            />
+          </View>
+        )}
+        keyExtractor={item => String(item.id)}
+        onScroll={handleScorll}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        initialScrollIndex={initialIndex}
+        onScrollToIndexFailed={() => {
+          setInitialIndex(0);
+        }}
+        getItemLayout={(_, index) => ({
+          length: deviceWidth,
+          offset: deviceWidth * index,
+          index,
+        })} // 최적화
+      />
+
+      <View style={[styles.pageContainer, {bottom: inset.bottom + 10}]}>
+        {Array.from({length: images.length}, (_, index) => (
+          <View
+            key={index}
+            style={[styles.pageDot, index === page && styles.currentPageDot]}
+          />
+        ))}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: colors.WHITE,
+  },
+  backButton: {
+    position: 'absolute',
+    left: 20,
+    zIndex: 1,
+    backgroundColor: colors.PINK_700,
+    width: 40,
+    height: 40,
+    borderRadius: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  pageContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    position: 'absolute',
+  },
+  pageDot: {
+    margin: 4,
+    backgroundColor: colors.GRAY_200,
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+  },
+  currentPageDot: {
+    backgroundColor: colors.PINK_700,
+  },
+});
+
+export default ImageCarousel;
