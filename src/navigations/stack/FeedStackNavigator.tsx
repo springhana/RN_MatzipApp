3484ae9b50fb92eec1a41ff3c555325ba76341e7
springhana <xksxksanfro7@naver.com>
2024-05-21 02:48:39 +0900

    ✨feat:  피드 관련 스크린 추가

--- src/navigations/stack/FeedStackNavigator.tsx
+++ src/navigations/stack/FeedStackNavigator.tsx
@@ -1,11 +1,17 @@
 import {createStackNavigator} from '@react-navigation/stack';
-import {feedNavigations} from '@/constants';
+import {colors, feedNavigations} from '@/constants';
 import FeedHomeScreen from '@/screens/feed/FeedHomeScreen';
 import FeedHomeHeaderLeft from '@/components/feed/FeedHomeHeaderLeft';
+import FeedDetailScreen from '@/screens/feed/FeedDetailScreen';
+import {LatLng} from 'react-native-maps';
+import EditPostScreen from '@/screens/feed/EditPostScreen';
+import ImageZoomScreen from '@/screens/feed/ImageZoomScreen';
 
 export type FeedStackParamList = {
   [feedNavigations.FEED_HOME]: undefined;
-  //   [feedNavigations.FEED_DETAIL]: undefined;
+  [feedNavigations.FEED_DETAIL]: {id: number};
+  [feedNavigations.EDIT_POST]: {location: LatLng};
+  [feedNavigations.IMAGE_ZOOM]: {index: number};
 };
 
 const Stack = createStackNavigator<FeedStackParamList>();
@@ -33,6 +39,32 @@ function FeedStackNavigator() {
           headerTitle: '피드',
           headerLeft: () => FeedHomeHeaderLeft(navigation),
         })}
+      />
+      <Stack.Screen
+        name={feedNavigations.FEED_DETAIL}
+        component={FeedDetailScreen}
+        options={{
+          headerShown: false,
+          headerTitle: ' ',
+          cardStyle: {
+            backgroundColor: colors.GRAY_100,
+          },
+        }}
+      />
+      <Stack.Screen
+        name={feedNavigations.EDIT_POST}
+        component={EditPostScreen}
+        options={{
+          headerTitle: '장소 수정',
+        }}
+      />
+      <Stack.Screen
+        name={feedNavigations.IMAGE_ZOOM}
+        component={ImageZoomScreen}
+        options={{
+          headerTitle: ' ',
+          headerShown: false,
+        }}
       />
     </Stack.Navigator>
   );
