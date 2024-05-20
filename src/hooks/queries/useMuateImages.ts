dbd99bc5449c80c50171ee481b5ae75331fea089
springhana <xksxksanfro7@naver.com>
2024-05-21 02:45:30 +0900

    ✨feat:  이미지  mutate 추가

--- src/hooks/queries/useMuateImages.ts
+++ src/hooks/queries/useMuateImages.ts
@@ -1,8 +1,8 @@
 import {uploadImages} from '@/api';
-import {UseMutationOptionsCustomOptions} from '@/types';
+import {UseMutationCustomOptions} from '@/types';
 import {useMutation} from '@tanstack/react-query';
 
-function useMuateImages(mutationOptions?: UseMutationOptionsCustomOptions) {
+function useMuateImages(mutationOptions?: UseMutationCustomOptions) {
   return useMutation({
     mutationFn: uploadImages,
     ...mutationOptions,
