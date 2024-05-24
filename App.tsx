7f2130546c276eb6a42a34783b5de3d7697e7e10
springhana <xksxksanfro7@naver.com>
2024-05-25 00:34:17 +0900

    ✨feat:  테마 적용

--- App.tsx
+++ App.tsx
@@ -2,12 +2,54 @@ import {NavigationContainer} from '@react-navigation/native';
 import RootNavigator from './src/navigations/root/RootNavigator';
 import {QueryClientProvider} from '@tanstack/react-query';
 import queryClient from './src/api/queryClient';
+import Toast, {
+  BaseToast,
+  ErrorToast,
+  BaseToastProps,
+} from 'react-native-toast-message';
+import {StatusBar} from 'react-native';
+import {colors} from '@/constants';
+import useThemeStorage from '@/hooks/useThemeStorage';
+
+const toastConfig = {
+  success: (props: BaseToastProps) => (
+    <BaseToast
+      {...props}
+      style={{borderLeftColor: colors['light'].BLUE_500}}
+      text1Style={{
+        fontSize: 14,
+      }}
+      text2Style={{
+        fontSize: 12,
+      }}
+    />
+  ),
+
+  error: (props: BaseToastProps) => (
+    <ErrorToast
+      {...props}
+      style={{borderLeftColor: colors['light'].RED_500}}
+      text1Style={{
+        fontSize: 14,
+      }}
+      text2Style={{
+        fontSize: 12,
+      }}
+    />
+  ),
+};
 
 function App(): JSX.Element {
+  const {theme} = useThemeStorage();
+
   return (
     <QueryClientProvider client={queryClient}>
+      <StatusBar
+        barStyle={theme === 'light' ? 'dark-content' : 'light-content'}
+      />
       <NavigationContainer>
         <RootNavigator />
+        <Toast config={toastConfig} />
       </NavigationContainer>
     </QueryClientProvider>
   );
