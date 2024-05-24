d457df1d0359f78a4e22b63ea6013793c938bf2c
springhana <xksxksanfro7@naver.com>
2024-05-25 02:00:18 +0900

    🐛fix:  오타 수정

--- src/hooks/queries/useGetCalendarPosts.ts
+++ src/hooks/queries/useGetCalendarPosts.ts
@@ -1,4 +1,4 @@
-import {ResponseCalendarPost, getCalendarPost} from '@/api';
+import {ResponseCalendarPost, getCalendarPosts} from '@/api';
 import {queryKeys} from '@/constants';
 import {UseQueryCustomOptions} from '@/types';
 import {keepPreviousData, useQuery} from '@tanstack/react-query';
@@ -9,7 +9,7 @@ function useGetCalendarPosts(
   queryOptions?: UseQueryCustomOptions<ResponseCalendarPost>,
 ) {
   return useQuery({
-    queryFn: () => getCalendarPost(year, month),
+    queryFn: () => getCalendarPosts(year, month),
     queryKey: [queryKeys.POST, queryKeys.GET_CALENDAR_POSTS, year, month],
     placeholderData: keepPreviousData, // 데이터를 가져오는 동안 이전 데이터를 유지해준다.
     ...queryOptions,
