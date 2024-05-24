5327ad9d92a1325981781d6dae1e96d69f044de4
springhana <xksxksanfro7@naver.com>
2024-05-25 01:02:08 +0900

    ✨feat:  달력 스크린 부분 추가

---
+++ src/components/calendar/EventList.tsx
@@ -0,0 +1,103 @@
+import {CalendarPost} from '@/api';
+import {
+  colors,
+  feedNavigations,
+  feedTabNavigations,
+  mainNavigations,
+} from '@/constants';
+import {StyleSheet, ScrollView, View, Pressable, Text} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {useNavigation, CompositeNavigationProp} from '@react-navigation/native';
+import {DrawerNavigationProp} from '@react-navigation/drawer';
+import {MainDrawerParamList} from '@/navigations/drawer/MainDrawerNavigator';
+import {BottomTabNavigationProp} from '@react-navigation/bottom-tabs';
+import {FeedTabParamList} from '@/navigations/tab/FeedTabNavigator';
+import useThemeStore from '@/store/useThemeStore';
+import {ThemeMode} from '@/types';
+
+interface EventListProps {
+  posts: CalendarPost[];
+}
+
+type Navigation = CompositeNavigationProp<
+  DrawerNavigationProp<MainDrawerParamList>,
+  BottomTabNavigationProp<FeedTabParamList>
+>;
+
+const EventList = ({posts}: EventListProps) => {
+  const {theme} = useThemeStore();
+  const styles = styling(theme);
+
+  const navigation = useNavigation<Navigation>();
+  const insets = useSafeAreaInsets();
+
+  const handlePressItem = (id: number) => {
+    navigation.navigate(mainNavigations.FEED, {
+      screen: feedTabNavigations.FEED_HOME,
+      params: {
+        screen: feedNavigations.FEED_DETAIL,
+        params: {id},
+        initial: false,
+      },
+    });
+  };
+
+  return (
+    <ScrollView style={styles.container} scrollIndicatorInsets={{right: 1}}>
+      <View style={[styles.innerContainer, {marginBottom: insets.bottom + 30}]}>
+        {posts?.map(post => (
+          <Pressable
+            key={post.id}
+            style={styles.itemContainer}
+            onPress={() => handlePressItem(post.id)}>
+            <View style={styles.itemHeader} />
+            <View style={styles.infoContainer}>
+              <Text
+                style={styles.addressText}
+                numberOfLines={1}
+                ellipsizeMode="tail">
+                {post.address}
+              </Text>
+              <Text style={styles.titleText}>{post.title}</Text>
+            </View>
+          </Pressable>
+        ))}
+      </View>
+    </ScrollView>
+  );
+};
+
+const styling = (theme: ThemeMode) =>
+  StyleSheet.create({
+    container: {
+      backgroundColor: colors[theme].WHITE,
+      padding: 20,
+    },
+    innerContainer: {
+      gap: 20,
+    },
+    itemContainer: {
+      flexDirection: 'row',
+    },
+    itemHeader: {
+      backgroundColor: colors[theme].PINK_700,
+      width: 6,
+      height: 50,
+      marginRight: 8,
+      borderRadius: 20,
+    },
+    infoContainer: {
+      justifyContent: 'space-evenly',
+    },
+    addressText: {
+      color: colors[theme].GRAY_500,
+      fontSize: 13,
+    },
+    titleText: {
+      color: colors[theme].BLACK,
+      fontSize: 16,
+      fontWeight: '600',
+    },
+  });
+
+export default EventList;
