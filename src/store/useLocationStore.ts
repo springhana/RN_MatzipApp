3e596078c2b3bd3d617ca9a24da4b36c840f22a9
springhana <xksxksanfro7@naver.com>
2024-05-25 00:44:19 +0900

    🔨modify:  선택된 위치 추가

--- src/store/useLocationStore.ts
+++ src/store/useLocationStore.ts
@@ -1,15 +1,21 @@
-import {LatLng} from 'react-native-maps';
 import {create} from 'zustand';
+import {LatLng} from 'react-native-maps';
 
 interface LocationState {
   moveLocation: LatLng | null;
-  setMoveLocation: (location: LatLng) => void;
+  selectLocation: LatLng | null;
+  setMoveLocation: (location: LatLng | null) => void;
+  setSelectLocation: (location: LatLng | null) => void;
 }
 
 const useLocationStore = create<LocationState>(set => ({
   moveLocation: null,
-  setMoveLocation: (moveLocation: LatLng) => {
-    set({moveLocation});
+  selectLocation: null,
+  setMoveLocation: (moveLocation: LatLng | null) => {
+    set(state => ({...state, moveLocation}));
+  },
+  setSelectLocation: (selectLocation: LatLng | null) => {
+    set(state => ({...state, selectLocation}));
   },
 }));
 
