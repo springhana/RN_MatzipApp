cfcd665b7ab47d916e067a6da03090f7a67da2a9
springhana <xksxksanfro7@naver.com>
2024-05-25 00:53:39 +0900

    ✨feat:  모두 추가 - 멀티인지 싱글인지

--- src/hooks/useImagePicker.ts
+++ src/hooks/useImagePicker.ts
@@ -1,15 +1,24 @@
-import {getFormDataImages} from '@/utils';
-import ImagePicker from 'react-native-image-crop-picker';
-import useMuateImages from './queries/useMuateImages';
 import {useState} from 'react';
-import {ImageUri} from '@/types';
 import {Alert} from 'react-native';
+import Toast from 'react-native-toast-message';
+import ImagePicker from 'react-native-image-crop-picker';
+
+import useMuateImages from './queries/useMuateImages';
+
+import {getFormDataImages} from '@/utils';
+import {ImageUri} from '@/types';
 
 interface UseImagePickerProps {
   initialImage: ImageUri[];
+  mode?: 'multiple' | 'single';
+  onSettled?: () => void;
 }
 
-function useImagePicker({initialImage = []}: UseImagePickerProps) {
+function useImagePicker({
+  initialImage = [],
+  mode = 'multiple',
+  onSettled,
+}: UseImagePickerProps) {
   const [imageUris, setImageUris] = useState(initialImage);
   const uploadImages = useMuateImages();
 
@@ -21,24 +30,41 @@ function useImagePicker({initialImage = []}: UseImagePickerProps) {
     setImageUris(prev => [...prev, ...uris.map(uri => ({uri}))]);
   };
 
+  const replaceImageUri = (uris: string[]) => {
+    if (uris.length > 1) {
+      Alert.alert('이미지 개수 초과', '추가 가능한 이미지는 최대 1개입니다.');
+      return;
+    }
+
+    setImageUris([...uris.map(uri => ({uri}))]);
+  };
+
   const handleChange = () => {
     ImagePicker.openPicker({
       mediaType: 'photo',
       multiple: true,
       includeBase64: true,
-      maxFiles: 5,
+      maxFiles: mode === 'multiple' ? 5 : 1,
       cropperChooseText: '완료',
       cropperCancelText: '취소',
     })
       .then(images => {
         const formData = getFormDataImages(images);
         uploadImages.mutate(formData, {
-          onSuccess: data => addImageUris(data),
+          onSuccess: data =>
+            mode === 'multiple' ? addImageUris(data) : replaceImageUri(data),
+          onSettled: () => onSettled && onSettled(),
         });
       })
       .catch(error => {
         if (error.code !== 'E_PICKER_CANCELLED') {
           // 에러메시지 표시
+          Toast.show({
+            type: 'error',
+            text1: '갤러리를 열 수 없어요',
+            text2: '권한을 허용했는지 확인해주세요.',
+            position: 'bottom',
+          });
         }
       });
   };
