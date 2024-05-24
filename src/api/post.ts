6a6fe2fa1de44e44a1b9c48fec8e28f33dc666ea
springhana <xksxksanfro7@naver.com>
2024-05-25 00:35:15 +0900

    ✨feat:  카카오, 애플, 계정 삭제, 계정 수정 추가

--- src/api/post.ts
+++ src/api/post.ts
@@ -58,6 +58,23 @@ const updateFavoritePost = async (id: number): Promise<number> => {
   return data;
 };
 
+type CalendarPost = {
+  id: number;
+  title: string;
+  address: string;
+};
+
+type ResponseCalendarPost = Record<number, CalendarPost[]>;
+
+const getCalendarPost = async (
+  year: number,
+  month: number,
+): Promise<ResponseCalendarPost> => {
+  const {data} = await axiosInstance.get(`/posts?year=${year}&month=${month}`);
+
+  return data;
+};
+
 export {
   createPost,
   getPost,
@@ -66,10 +83,13 @@ export {
   updatePost,
   updateFavoritePost,
   getFavoritePosts,
+  getCalendarPost,
 };
 export type {
   ResponsePost,
   RequestCreatePost,
   ResponseSinglePost,
   RequestUpdatePost,
+  CalendarPost,
+  ResponseCalendarPost,
 };
