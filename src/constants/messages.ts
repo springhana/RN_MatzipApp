3b9d37c23fe01ff294975edc5ca7016a24e618e9
springhana <xksxksanfro7@naver.com>
2024-05-25 00:49:27 +0900

    ✨feat:  에러 메시지 추가

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,5 +1,6 @@
 const errorMessages = {
   CANNOT_GET_ADDRESS: '주소를 알 수 없습니다.',
+  UNEXPECT_ERROR: '알 수 없는 에러가 발생했습니다.',
 } as const;
 
 const alerts = {
@@ -18,6 +19,10 @@ const alerts = {
   DELETE_POST: {
     TITLE: '삭제하시겠습니까?',
     DESCRIPTION: '피드와 지도에서 모두 삭제됩니다.',
+  },
+  DELETE_ACCOUNT: {
+    TITLE: '정말 탈퇴하시겠습니까?',
+    DESCRIPTION: '회원 정보는 삭제되며, 복구할 수 없습니다.',
   },
 } as const;
 
