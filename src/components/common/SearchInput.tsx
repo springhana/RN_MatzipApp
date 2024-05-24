a473b971aa508c56e2734b220c03c540900235cc
springhana <xksxksanfro7@naver.com>
2024-05-25 01:01:43 +0900

    ✨feat:  주소 검색 스크린 부분 추가

---
+++ src/components/common/SearchInput.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {StyleSheet, TextInput, TextInputProps, View} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import {colors} from '@/constants';
+import useThemeStore from '@/store/useThemeStore';
+import {ThemeMode} from '@/types';
+
+interface SearchInputProps extends TextInputProps {
+  onSubmit: () => void;
+}
+
+function SearchInput({onSubmit, ...props}: SearchInputProps) {
+  const {theme} = useThemeStore();
+  const styles = styling(theme);
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        placeholderTextColor={colors[theme].GRAY_500}
+        returnKeyType="search"
+        onSubmitEditing={onSubmit}
+        clearButtonMode="while-editing"
+        {...props}
+      />
+      <Ionicons
+        name={'search'}
+        color={colors[theme].GRAY_700}
+        size={20}
+        onPress={onSubmit}
+      />
+    </View>
+  );
+}
+
+const styling = (theme: ThemeMode) =>
+  StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      borderWidth: 1,
+      borderColor: colors[theme].GRAY_200,
+      paddingVertical: 8,
+      paddingHorizontal: 10,
+      borderRadius: 5,
+    },
+    input: {
+      flex: 1,
+      fontSize: 16,
+      paddingVertical: 0,
+      paddingLeft: 0,
+      color: colors[theme].BLACK,
+    },
+  });
+
+export default SearchInput;
---
+++ src/components/map/SearchRegionResult.tsx
@@ -0,0 +1,142 @@
+import {colors} from '@/constants';
+import {RegionInfo} from '@/hooks/useSearchLocatin';
+import useLocationStore from '@/store/useLocationStore';
+import useThemeStore from '@/store/useThemeStore';
+import {ThemeMode} from '@/types';
+import {useNavigation} from '@react-navigation/native';
+import React from 'react';
+import {Dimensions, Pressable, ScrollView, Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {LatLng} from 'react-native-maps';
+import Octicons from 'react-native-vector-icons/Octicons';
+
+interface SearchRegionResultProps {
+  regionInfo: RegionInfo[];
+}
+
+function SearchRegionResult({regionInfo}: SearchRegionResultProps) {
+  const {theme} = useThemeStore();
+  const styles = styling(theme);
+  const navigation = useNavigation();
+  const {setMoveLocation, setSelectLocation} = useLocationStore();
+
+  const handlePressRegionInfo = (latitude: string, longitude: string) => {
+    const regionLocation = {
+      latitude: Number(latitude),
+      longitude: Number(longitude),
+    };
+
+    moveToMapScreen(regionLocation);
+  };
+
+  const moveToMapScreen = (regionLocation: LatLng) => {
+    navigation.goBack();
+
+    setMoveLocation(regionLocation);
+    setSelectLocation(regionLocation);
+  };
+
+  return (
+    <View style={styles.container}>
+      <ScrollView
+        showsVerticalScrollIndicator
+        indicatorStyle="black"
+        contentContainerStyle={styles.scrollContainer}>
+        {regionInfo.map((info, index) => (
+          <Pressable
+            key={info.id}
+            style={[
+              styles.itemBorder,
+              index === regionInfo.length - 1 && styles.noItemBorder,
+            ]}
+            onPress={() => handlePressRegionInfo(info.y, info.x)}>
+            <View style={styles.placeNameContainer}>
+              <Octicons
+                name="location"
+                size={15}
+                color={colors[theme].PINK_700}
+              />
+              <Text
+                style={styles.placeText}
+                ellipsizeMode="tail"
+                numberOfLines={1}>
+                {info.place_name}
+              </Text>
+            </View>
+            <View style={styles.categoryContainer}>
+              <Text style={styles.distanceText}>
+                {(Number(info.distance) / 1000).toFixed(2)}km
+              </Text>
+              <Text style={styles.subInfoText}>{info.category_name}</Text>
+            </View>
+            <Text style={styles.subInfoText}>{info.road_address_name}</Text>
+          </Pressable>
+        ))}
+
+        {regionInfo.length === 0 && (
+          <View style={styles.noResultContainer}>
+            <Text style={styles.noResultText}>검색 결과가 없습니다.</Text>
+          </View>
+        )}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styling = (theme: ThemeMode) =>
+  StyleSheet.create({
+    container: {
+      borderWidth: 1,
+      borderColor: colors[theme].GRAY_200,
+      borderRadius: 5,
+      height: Dimensions.get('screen').height / 2,
+      marginVertical: 5,
+      width: '100%',
+    },
+    scrollContainer: {
+      padding: 10,
+    },
+    placeNameContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      gap: 5,
+    },
+    placeText: {
+      color: colors[theme].BLACK,
+      flexShrink: 1,
+      fontSize: 16,
+      fontWeight: '600',
+    },
+    categoryContainer: {
+      flexDirection: 'row',
+      gap: 10,
+    },
+    distanceText: {
+      color: colors[theme].BLACK,
+    },
+    subInfoText: {
+      flexShrink: 1,
+      color: colors[theme].GRAY_500,
+    },
+    itemBorder: {
+      marginHorizontal: 5,
+      paddingVertical: 10,
+      borderBottomColor: colors[theme].GRAY_300,
+      borderBottomWidth: StyleSheet.hairlineWidth,
+      gap: 3,
+    },
+    noItemBorder: {
+      borderBottomWidth: 0,
+    },
+    noResultContainer: {
+      flex: 1,
+      alignItems: 'center',
+      marginTop: 50,
+    },
+    noResultText: {
+      color: colors[theme].GRAY_500,
+      fontSize: 16,
+    },
+  });
+
+export default SearchRegionResult;
