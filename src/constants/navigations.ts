ad202e7de7f58e4eb7aabb088cef5da9d3fe5ad1
springhana <xksxksanfro7@naver.com>
2024-05-25 00:49:57 +0900

    ✨feat:  설정 네비게이션 추가

--- src/constants/navigations.ts
+++ src/constants/navigations.ts
@@ -2,17 +2,20 @@ const mainNavigations = {
   HOME: 'Home',
   FEED: 'Feed',
   CALENDAR: 'Calendar',
+  SETTING: 'Setting',
 } as const;
 
 const authNavigations = {
   AUTH_HOME: 'AuthHome',
   LOGIN: 'Login',
   SIGNUP: 'Signup',
+  KAKAO: 'Kakao',
 } as const;
 
 const mapNavigations = {
   MAP_HOME: 'MapHome',
   ADD_POST: 'AddPost',
+  SEARCH_LOCATION: 'SearchLocation',
 } as const;
 
 const feedNavigations = {
@@ -27,10 +30,18 @@ const feedTabNavigations = {
   FEED_FAVORITE: 'FeedFavorite',
 } as const;
 
+const settingNavigations = {
+  SETTING_HOME: 'SettingHome',
+  EDIT_PROFILE: 'EditProfile',
+  DELETE_ACCOUNT: 'DeleteAccount',
+  EDIT_CATEGORY: 'EditCategory',
+} as const;
+
 export {
   mainNavigations,
   authNavigations,
   mapNavigations,
   feedNavigations,
   feedTabNavigations,
+  settingNavigations,
 };
