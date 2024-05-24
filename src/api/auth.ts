f2fd23ebb2a7095190054c8869ec1cff05bb8ad7
springhana <xksxksanfro7@naver.com>
2024-05-25 00:34:58 +0900

    ✨feat:  카카오, 애플, 계정 삭제, 계정 수정 추가

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -34,10 +34,41 @@ const postLogin = async ({
   return data;
 };
 
+const kakaoLogin = async (token: string): Promise<ResponseToken> => {
+  const {data} = await axiosInstance.post(`/auth/oauth/kakao`, {token});
+
+  return data;
+};
+
+type RequestAppleIdentity = {
+  identityToken: string;
+  appId: string;
+  nickname: string | null;
+};
+
+const appleLogin = async (
+  body: RequestAppleIdentity,
+): Promise<ResponseToken> => {
+  const {data} = await axiosInstance.post(`/auth/oauth/apple`, body);
+
+  return data;
+};
+
 type ResponseProfile = Profile & Category;
 
 const getProfile = async (): Promise<ResponseProfile> => {
   const {data} = await axiosInstance.get('/auth/me');
+
+  return data;
+};
+
+type RequestProfile = Omit<
+  Profile,
+  'id' | 'email' | 'kakaoImageUri' | 'loginType'
+>;
+
+const editProfile = async (body: RequestProfile): Promise<ResponseProfile> => {
+  const {data} = await axiosInstance.patch(`/auth/me`, body);
 
   return data;
 };
@@ -58,5 +89,32 @@ const logout = async () => {
   await axiosInstance.post(`/auth/logout`);
 };
 
-export {postSignup, postLogin, getProfile, getAccessToken, logout};
-export type {RequestUser, ResponseToken, ResponseProfile};
+const deleteAccount = async () => {
+  await axiosInstance.delete(`/auth/me`);
+};
+
+const editCategory = async (body: Category): Promise<ResponseProfile> => {
+  const {data} = await axiosInstance.patch(`/auth/category`, body);
+
+  return data;
+};
+
+export {
+  postSignup,
+  postLogin,
+  getProfile,
+  getAccessToken,
+  logout,
+  kakaoLogin,
+  appleLogin,
+  editProfile,
+  deleteAccount,
+  editCategory,
+};
+export type {
+  RequestUser,
+  ResponseToken,
+  ResponseProfile,
+  RequestAppleIdentity,
+  RequestProfile,
+};
