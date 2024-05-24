9e42ff8eb199b525ae97f592aa79ee940fe3c6b3
springhana <xksxksanfro7@naver.com>
2024-05-25 01:03:46 +0900

    ✨feat:  설정 부분 추가

--- src/navigations/drawer/CustomDrawerContent.tsx
+++ src/navigations/drawer/CustomDrawerContent.tsx
@@ -1,6 +1,6 @@
-import {StyleSheet, View, Text, Image, Pressable} from 'react-native';
+import {StyleSheet, View, Text, Image, Pressable, Platform} from 'react-native';
 
-import {colors} from '@/constants';
+import {colors, mainNavigations, settingNavigations} from '@/constants';
 import useAuth from '@/hooks/queries/useAuth';
 import {
   DrawerContentComponentProps,
@@ -8,15 +8,22 @@ import {
   DrawerItemList,
 } from '@react-navigation/drawer';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Config from 'react-native-config';
+import {ThemeMode} from '@/types';
+import useThemeStore from '@/store/useThemeStore';
 
 function CustomDrawerContent(props: DrawerContentComponentProps) {
-  const {logoutMutation, getProfileQuery} = useAuth();
+  const {theme} = useThemeStore();
+  const styles = styling(theme);
+  const {getProfileQuery} = useAuth();
   const {email, nickname, imageUri, kakaoImageUri} = getProfileQuery.data || {};
 
-  const handleLogout = () => {
-    logoutMutation.mutate(null);
+  const handlePressSetting = () => {
+    props.navigation.navigate(mainNavigations.SETTING, {
+      screen: settingNavigations.SETTING_HOME,
+    });
   };
-
   return (
     <SafeAreaView style={styles.container}>
       <DrawerContentScrollView
@@ -32,56 +39,95 @@ function CustomDrawerContent(props: DrawerContentComponentProps) {
               />
             )}
             {imageUri !== null && !!kakaoImageUri && (
-              <Image source={{uri: kakaoImageUri}} style={styles.userImage} />
+              <Image
+                source={{
+                  uri: `${
+                    Platform.OS === 'ios'
+                      ? 'http://localhost:3030'
+                      : Config.IPCONFIG
+                  }/${kakaoImageUri}`,
+                }}
+                style={styles.userImage}
+              />
             )}
             {imageUri !== null && (
-              <Image source={{uri: imageUri}} style={styles.userImage} />
+              <Image
+                source={{
+                  uri: `${
+                    Platform.OS === 'ios'
+                      ? 'http://localhost:3030'
+                      : Config.IPCONFIG
+                  }/${imageUri}`,
+                }}
+                style={styles.userImage}
+              />
             )}
           </View>
           <Text style={styles.nameText}>{nickname ?? email}</Text>
         </View>
         <DrawerItemList {...props} />
 
-        <Pressable
-          onPress={handleLogout}
-          style={{
-            alignItems: 'flex-end',
-            padding: 10,
-          }}>
-          <Text>로그아웃</Text>
-        </Pressable>
+        <View style={styles.bottomContainer}>
+          <Pressable style={styles.bottomMenu} onPress={handlePressSetting}>
+            <MaterialIcons
+              name={'settings'}
+              color={colors[theme].GRAY_700}
+              size={18}
+            />
+            <Text style={styles.bottomMenuText}>설정</Text>
+          </Pressable>
+        </View>
       </DrawerContentScrollView>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  contentContainer: {
-    backgroundColor: colors.WHITE,
-  },
-  nameText: {
-    color: colors.BLACK,
-  },
-  userInfoContainer: {
-    alignItems: 'center',
-    marginTop: 15,
-    marginBottom: 30,
-    marginHorizontal: 15,
-  },
-  userImageContainer: {
-    width: 70,
-    height: 70,
-    borderRadius: 35,
-    marginBottom: 10,
-  },
-  userImage: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 35,
-  },
-});
+const styling = (theme: ThemeMode) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    contentContainer: {
+      backgroundColor: colors[theme].WHITE,
+    },
+    nameText: {
+      color: colors[theme].BLACK,
+    },
+    userInfoContainer: {
+      alignItems: 'center',
+      marginTop: 15,
+      marginBottom: 30,
+      marginHorizontal: 15,
+    },
+    userImageContainer: {
+      width: 70,
+      height: 70,
+      borderRadius: 35,
+      marginBottom: 10,
+    },
+    userImage: {
+      width: '100%',
+      height: '100%',
+      borderRadius: 35,
+    },
+    bottomContainer: {
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+      paddingHorizontal: 20,
+      paddingVertical: 15,
+      borderTopWidth: 1,
+      borderTopColor: colors[theme].GRAY_200,
+    },
+    bottomMenu: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      gap: 5,
+    },
+    bottomMenuText: {
+      fontWeight: '600',
+      fontSize: 15,
+      color: colors[theme].GRAY_700,
+    },
+  });
 
 export default CustomDrawerContent;
